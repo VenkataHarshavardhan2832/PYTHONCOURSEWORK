2414e2f5ad33b69b5ed5635756b14e18ab7db148
amrathhugo <amrath@hugohub.com>
2025-06-13 17:01:06 +0530

    Practice codes question1 and question2

---
+++ class1/practice_codes/question3.py
@@ -0,0 +1,13 @@
+'''Problem Statement:
+
+Write a Python program that asks the user to enter a number. Then tell them if the number is:
+
+Even or Odd
+
+Positive, Negative, or Zero
+sample output:
+Enter a number: -3
+The number -3 is odd and negative.
+
+'''
+
