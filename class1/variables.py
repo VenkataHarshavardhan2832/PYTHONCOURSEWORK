ef7103ae4ae4693f5906d99b1db1b8dad8a8c5c5
amrathhugo <amrath@hugohub.com>
2025-06-13 16:48:46 +0530

    Class 1 files added

---
+++ class1/variables.py
@@ -0,0 +1,19 @@
+# single line comment 
+# multi line comments
+
+
+# Single line comments starts with a hash symbol
+
+'''Multi-line comments are enclosed in triple quotes
+you can write as many lines as you want
+'''
+
+# variables are buckets which store data(Store data in the memory)
+
+# numberical variables - integers(int), decimals(float) , complex numbers(complex)
+
+# string variables - text data(str)
+
+# boolean variables - True(1) or False(0) (bool)
+
+# list variables - list [1,2,3,4,5,6] , tuple, set,dict
