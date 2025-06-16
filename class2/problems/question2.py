3f464071dcbe038fd15d6ae18bcb7c4a3242ae86
VenkataHarshavardhan2832 <maddineniharshavardhan767@gmail.com>
2025-06-17 00:24:04 +0530

    question1 and question2 sol's

--- class2/problems/question2.py
+++ class2/problems/question2.py
@@ -8,3 +8,22 @@ Now sort the resulting list in descending order and print the sorted list.
 
 and create the list from the resulting list with starting 5 values and remove the last value from the list and first occurence of 100 from the list
 '''
+scores = [100, 10, 30, 60, 90,40,60]
+scores.append(50)
+scores.append(80)
+scores.append(70)
+
+#extend it with scores_2
+scores_2 = [70, 80, 50,100]
+scores_2.extend(scores_2)
+
+#The resulting list in descending order
+scores.sort(reverse=True)
+print(scores)
+
+#create the list from the resulting list with starting 5 values and remove the last value from the list and first occurence of 100 from the list
+starting_scores = scores[:5]
+scores.pop()
+if 100 in starting_scores:
+    starting_scores.remove(100)
+print(starting_scores)
