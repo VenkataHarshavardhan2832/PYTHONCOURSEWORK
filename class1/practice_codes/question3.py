eabc45cbd873882020c738b402e5782b732db94c
VenkataHarshavardhan2832 <maddineniharshavardhan767@gmail.com>
2025-06-16 09:30:04 +0530

    question2 and question3 sol

--- class1/practice_codes/question3.py
+++ class1/practice_codes/question3.py
@@ -10,4 +10,15 @@ Enter a number: -3
 The number -3 is odd and negative.
 
 '''
+number = int(input("Enter a number: "))
 
+if number == 0:
+    print("The %d is zero and even" %number )
+elif number > 0 and number % 2 == 0:
+    print("The %d is even and positive" %number)
+elif number > 0 and number % 2 != 0:
+    print("The {} is odd and positive".format(number))
+elif number < 0 and number % 2 == 0:
+    print("The {} is even and negative".format(number))
+else:
+    print("The {} is odd and negative".format(number))
