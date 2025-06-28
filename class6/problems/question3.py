7e0228660cafe7f7e62b90c19913ccca01a6c3a5
amrathhugo <amrath@hugohub.com>
2025-06-28 12:58:35 +0530

    class6 notes and problems on sets and edictionaries

---
+++ class6/problems/question3.py
@@ -0,0 +1,6 @@
+'''
+Remove distinct elements from a list
+
+arr = [1,2,3,4,1,2,3]
+output: [1,2,3,4]
+'''
---
+++ class6/problems/question4.py
@@ -0,0 +1,7 @@
+'''
+Do all mathematic operations on a set - union,intersection,difference,symettric difference.
+
+CHeck if a set is subset of another set.
+
+Explore about frozen sets in python
+'''
