bf08d3bd22ff684142d415b43c3a662232636f85
amrathhugo <amrath@hugohub.com>
2025-07-03 18:20:06 +0530

    Some better solution for class6 question1

---
+++ class5/time_and_space_complexity.py
@@ -0,0 +1,12 @@
+# How we mesure the program we have written is optimized on memory and compute
+
+# time and space complexity
+
+# per 1 sec they can iterate through 10^8 numbers
+
+# leetcode or geeksforgeeks or codeforces
+# time and space complexity is measured with (n - number or size you are iterating)
+
+
+for i in range(pow(10,8)):
+    print(i,end=" ")
--- class6/problems/question1.py
+++ class6/problems/question1.py
@@ -8,3 +8,12 @@ s = input("Enter a string: ")
 for ch in s:
     if s.index(ch) == s.find(ch):  # to avoid printing duplicates
         print(ch, ":", s.count(ch), end=", ")
+
+
+dictionary = {}
+for ch in s:
+    if ch in dictionary.keys():
+        dictionary[ch] += 1
+    else:
+        dictionary[ch] = 1
+print(dictionary)
