2e2b9908e4b5acd5f78e307fed43b48b221eeb02
VenkataHarshavardhan2832 <maddineniharshavardhan767@gmail.com>
2025-07-03 18:39:08 +0530

    question5 solution

--- class6/problems/question5.py
+++ class6/problems/question5.py
@@ -5,3 +5,19 @@ example:
 Input : [1,2,3,4,1,1,1,2,3,4]
 answer : 1
 '''
+def most_repeating_element(arr):
+    count_dict={}
+    for number in arr:
+        if number in count_dict.keys():
+            count_dict[number] += 1
+        else:
+            count_dict[number] = 1
+    repeat = 0
+    answer = 0
+    for key, value in count_dict.items():
+        if value > repeat:
+            repeat = value
+            answer = key
+    return answer
+ans = most_repeating_element(arr=[1,2,3,4,3,1,3,2,3,4])
+print(ans)
